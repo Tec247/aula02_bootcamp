91f3ea9ae94be06707ee880d14643aca71ff2a58
Tec247 <pegazoz99@gmail.com>
2025-05-22 19:50:39 -0300

    adicionando o exercício 16

--- exercicios.py
+++ exercicios.py
@@ -100,6 +100,12 @@ print(f" A concatenação gerou o seguinte: {texto1} {texto2}")
 # #### Booleanos (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+valor1 = False
+valor2 = False
+resultado = valor1 and valor2
+print(f"O resultado entre a operação de OR é: {resultado}")
+
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
