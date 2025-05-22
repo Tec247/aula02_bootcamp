70beb357da588a7cb35656c28606b9b3d882da79
Tec247 <pegazoz99@gmail.com>
2025-05-21 21:29:45 -0300

    adicionando o exercicio 02

--- exercicios.py
+++ exercicios.py
@@ -8,6 +8,11 @@ result = numero1+numero2
 print(f"O resultado da soma é: {result}")
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+CONST_NUM = 5
+n = int(input("Digite um número: "))
+valor2 = n % CONST_NUM
+print(f"O resto da divisão pelo número {CONST_NUM} é {valor2}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
